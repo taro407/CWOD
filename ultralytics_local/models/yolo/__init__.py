b30fb4e90f9222988dfea57b35f10273d62eea06
2042829182 <2042829182@qq.com>
2026-05-08 17:14:52 +0800

    Delete ultralytics_local directory

--- ultralytics_local/models/yolo/__init__.py
+++
@@ -1,7 +0,0 @@
-# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
-
-from ultralytics_local.models.yolo import classify, detect, obb, pose, segment, world, yoloe
-
-from .model import YOLO, YOLOE, YOLOWorld
-
-__all__ = "classify", "segment", "detect", "pose", "obb", "world", "yoloe", "YOLO", "YOLOWorld", "YOLOE"
